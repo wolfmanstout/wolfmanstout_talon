231e8223a3a16ea78e4266f1db119cf8396916c7
James Stout <james.wolf.stout@gmail.com>
2022-09-18 20:35:23 -0700

    Use relative imports to fix type check in timestamped_captures_override.py

--- code/timestamped_captures_override.py
+++ code/timestamped_captures_override.py
@@ -1,12 +1,6 @@
-import importlib
-
 from talon import Context
 
-timestamped_captures = importlib.import_module(
-    "user.talon-gaze-ocr.timestamped_captures"
-)
-TimestampedText = timestamped_captures.TimestampedText
-
+from ...talon_gaze_ocr.timestamped_captures import TimestampedText
 
 ctx = Context()
 
@@ -14,8 +8,7 @@ ctx = Context()
 @ctx.capture(rule="<user.timestamped_phrase_default> | <user.prose_contact>")
 def timestamped_phrase(m) -> TimestampedText:
     item = m[0]
-    # HACK: check the name because the class object might be different due to a separate import.
-    if type(item).__name__ == "TimestampedText":
+    if isinstance(item, TimestampedText):
         return item
     else:
         return TimestampedText(text=str(item), start=item.start, end=item.end)
