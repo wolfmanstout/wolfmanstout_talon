c90dd9aa408aa2e4199acd0a1536980e752346e8
James Stout <james.wolf.stout@gmail.com>
2025-02-14 17:29:47 -0800

    Disable jetbrains.py, which breaks copy/paste unless an extension is installed.

--- apps/jetbrains/jetbrains.py
+++ apps/jetbrains/jetbrains.py
@@ -104,56 +104,55 @@ def get_idea_location() -> list[str]:
 ctx = Context()
 mod = Module()
 
-mod.apps.jetbrains = "app.name: /jetbrains/"
-mod.apps.jetbrains = "app.name: CLion"
-mod.apps.jetbrains = "app.name: IntelliJ IDEA"
-mod.apps.jetbrains = "app.name: PhpStorm"
-mod.apps.jetbrains = "app.name: PyCharm"
-mod.apps.jetbrains = "app.name: WebStorm"
-mod.apps.jetbrains = "app.name: RubyMine"
-mod.apps.jetbrains = "app.name: RubyMine-EAP"
-mod.apps.jetbrains = "app.name: DataGrip"
-mod.apps.jetbrains = """
-os: mac
-and app.bundle: com.google.android.studio
-"""
-# windows
-mod.apps.jetbrains = r"app.exe: /^idea64\.exe$/i"
-mod.apps.jetbrains = r"app.exe: /^PyCharm64\.exe$/i"
-mod.apps.jetbrains = r"app.exe: /^webstorm64\.exe$/i"
-mod.apps.jetbrains = """
-os: mac
-and app.bundle: com.jetbrains.pycharm
-"""
-mod.apps.jetbrains = """
-os: mac
-and app.bundle: com.jetbrains.rider
-"""
-mod.apps.jetbrains = """
-os: mac
-and app.bundle: com.jetbrains.goland
-"""
-mod.apps.jetbrains = """
-os: mac
-and app.bundle: com.jetbrains.intellij.ce
-"""
-mod.apps.jetbrains = r"""
-os: windows
-and app.name: JetBrains Rider
-os: windows
-and app.exe: /^rider64\.exe$/i
-"""
+# mod.apps.jetbrains = "app.name: /jetbrains/"
+# mod.apps.jetbrains = "app.name: CLion"
+# mod.apps.jetbrains = "app.name: IntelliJ IDEA"
+# mod.apps.jetbrains = "app.name: PhpStorm"
+# mod.apps.jetbrains = "app.name: PyCharm"
+# mod.apps.jetbrains = "app.name: WebStorm"
+# mod.apps.jetbrains = "app.name: RubyMine"
+# mod.apps.jetbrains = "app.name: RubyMine-EAP"
+# mod.apps.jetbrains = "app.name: DataGrip"
+# mod.apps.jetbrains = """
+# os: mac
+# and app.bundle: com.google.android.studio
+# """
+# # windows
+# mod.apps.jetbrains = r"app.exe: /^idea64\.exe$/i"
+# mod.apps.jetbrains = r"app.exe: /^PyCharm64\.exe$/i"
+# mod.apps.jetbrains = r"app.exe: /^webstorm64\.exe$/i"
+# mod.apps.jetbrains = """
+# os: mac
+# and app.bundle: com.jetbrains.pycharm
+# """
+# mod.apps.jetbrains = """
+# os: mac
+# and app.bundle: com.jetbrains.rider
+# """
+# mod.apps.jetbrains = """
+# os: mac
+# and app.bundle: com.jetbrains.goland
+# """
+# mod.apps.jetbrains = """
+# os: mac
+# and app.bundle: com.jetbrains.intellij.ce
+# """
+# mod.apps.jetbrains = r"""
+# os: windows
+# and app.name: JetBrains Rider
+# os: windows
+# and app.exe: /^rider64\.exe$/i
+# """
 
-# Local plugin development:
-mod.apps.jetbrains = """
-os: mac
-and app.bundle: com.jetbrains.jbr.java
-"""
+# # Local plugin development:
+# mod.apps.jetbrains = """
+# os: mac
+# and app.bundle: com.jetbrains.jbr.java
+# """
 
 
 @mod.action_class
 class Actions:
-
     def idea(commands: str):
         """Send a command to Jetbrains product"""
         command_list = commands.split(",")
@@ -300,7 +299,6 @@ class WinActions:
 
 @ctx.action_class("user")
 class UserActions:
-
     def command_server_directory() -> str:
         return "jetbrains-command-server"
 
