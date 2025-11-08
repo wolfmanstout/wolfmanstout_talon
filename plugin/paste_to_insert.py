eb6881e505e589ba96281ed364d9dff22728a739
James Stout <james.wolf.stout@gmail.com>
2025-11-07 18:11:02 -0800

    Always paste newlines when inserting text.

--- plugin/paste_to_insert.py
+++ plugin/paste_to_insert.py
@@ -1,5 +1,3 @@
-import logging
-
 from talon import Context, Module, actions, settings
 
 mod = Module()
@@ -17,9 +15,11 @@ Zero means always paste; -1 means never paste.
 
 @ctx.action_class("main")
 class MainActions:
-    def insert(text):
-        threshold = settings.get("user.paste_to_insert_threshold")
-        if 0 <= threshold < len(text):
+    def insert(text: str):
+        threshold: int = settings.get("user.paste_to_insert_threshold")  # type: ignore
+        # If paste_to_insert is available, always paste newlines to avoid editor
+        # handling of enter key (which could submit a form, for example).
+        if 0 <= threshold and (threshold < len(text) or "\n" in text):
             actions.user.paste(text)
             return
         actions.next(text)
