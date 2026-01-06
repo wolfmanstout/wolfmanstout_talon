f5b4caff559430679101b36d7bc9d680829ce152
James Stout <james.wolf.stout@gmail.com>
2026-01-05 20:46:48 -0800

    Use default line start/end for iterm2

--- apps/iterm/iterm.py
+++ apps/iterm/iterm.py
@@ -15,15 +15,6 @@ directories_to_remap = {}
 directories_to_exclude = {}
 
 
-@ctx.action_class("edit")
-class EditActions:
-    def line_start():
-        actions.key("home")
-
-    def line_end():
-        actions.key("end")
-
-
 @ctx.action_class("user")
 class UserActions:
     # def file_manager_current_path():
